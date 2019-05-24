a2c53a5ad3314a13c3fdb6c6168bd4c1282cf801
pedrynteam <Entrar123#>
2019-05-24 00:52:32 -0500

    First Release - Initial

---
+++ Mobile_Exchange/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mobile_Exchange.Models;
+
+namespace Mobile_Exchange.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Mobile_Exchange/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_Exchange.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
